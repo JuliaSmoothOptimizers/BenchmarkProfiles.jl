1e885dae432908a46ca1b68041c967b3333d7cfc
Dominique Orban <dominique.orban@gmail.com>
2016-02-26 08:34:21 -0500

    test data profiles

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,5 @@ using Base.Test
 unicodeplots()
 T = 10 * rand(25, 3)
 performance_profile(T, labels=["a", "b", "c"], title="Test Profile")
+H = rand(25, 4, 3)
+data_profile(H, ones(10), labels=["a", "b", "c"], title="Test Profile")
