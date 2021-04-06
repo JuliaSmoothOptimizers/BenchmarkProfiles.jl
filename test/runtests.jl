10b10a518f2b1291605e98611ad73525dd8bed15
Luiz-Rafael Santos <lrsantos11@gmail.com>
2021-04-06 15:10:54 -0300

    adds `drawtol` to include a draw tolerance between solvers

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,8 @@ using Test
 unicodeplots()
 T = 10 * rand(25, 3)
 performance_profile(T, ["a", "b", "c"], title="Test Profile", linestyles=[:solid, :dash, :dot])
+
+performance_profile(T, ["a", "b", "c"], drawtol = 0.05, title="Test Profile with drawtol", linestyles=[:solid, :dash, :dot])
 H = rand(25, 4, 3)
 data_profile(H, ones(10), ["a", "b", "c"], title="Test Profile")
 
