a3eac7024672abf04ec7aada5177869a67e3d62f
Luiz-Rafael Santos <lrsantos11@gmail.com>
2021-04-07 18:25:28 -0300

    Put DataFrames before Plots.

--- src/BenchmarkProfiles.jl
+++ src/BenchmarkProfiles.jl
@@ -1,9 +1,9 @@
 module BenchmarkProfiles
 
+using DataFrames
 import NaNMath
 using Requires
 using Printf
-using DataFrames
 
 export performance_ratios, performance_profile, performance_profile_data
 export data_ratios, data_profile
