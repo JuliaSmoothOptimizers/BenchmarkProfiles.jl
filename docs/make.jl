a62e37d0f810134fa50e18334ee63a58d493decc
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-07-01 17:35:27 -0300

    Remove strict from docs

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,6 @@ makedocs(
   modules = [BenchmarkProfiles],
   doctest = true,
   linkcheck = true,
-  strict = true,
   format = Documenter.HTML(assets = ["assets/style.css"], prettyurls = get(ENV, "CI", nothing) == "true"),
   sitename = "BenchmarkProfiles.jl",
   pages = ["Home" => "index.md",
