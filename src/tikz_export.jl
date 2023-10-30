b2327d7e6dbe1c17f346b8a09038e5c793f244fb
d-monnet <70266099+d-monnet@users.noreply.github.com>
2023-10-30 11:34:25 -0400

    Update src/tikz_export.jl
    Co-authored-by: tmigot <tangi.migot@gmail.com>

--- src/tikz_export.jl
+++ src/tikz_export.jl
@@ -22,7 +22,7 @@ Export tikz figure of the performance profiles given by `T` in `filename`.
 * `colours::Vector{String} = []` : colours of the plots, should have as many elements as the number of columns of `T`.
 * `linestyles::Vector{String} = []` : line style (dashed, dotted, ...) of the plots, should have as many elements as the number of columns of `T`.
 * `linewidth::AbstractFloat = 1.0` : line width of the plots.
-* `xlabel::String = ""` : x axis label. If empty, uses the one returns by `performance_profile_axis_labels`.
+* `xlabel::String = ""` : x-axis label. If empty, uses the one returned by `performance_profile_axis_labels`.
 * `ylabel::String = ""` : x axis label. If empty, uses the one returns by `performance_profile_axis_labels`.
 
 Other keyword arguments are passed `performance_profile_data`.
